4fdb0ff521adeeb828f406e85675bcb044536193
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-10-17 22:43:08 +0300

    String input added. Size of ocean input done

--- Battleship/src/battleship/BattleshipGame.java
+++ Battleship/src/battleship/BattleshipGame.java
@@ -2,19 +2,39 @@ package battleship;
 
 import battleship.utilities.InputHelper;
 
+import java.util.InputMismatchException;
+
 public class BattleshipGame {
     private static final InputHelper inputHelper = InputHelper.getInstance();
 
     public static void main(String[] args) {
         BattleshipGame gameSession = new BattleshipGame();
-        gameSession.playGame();
+        gameSession.playGame(args);
     }
 
-    public void playGame() {
+    public void playGame(String[] args) {
         System.out.println("Game started!");
 
-        System.out.println(inputHelper.parseInt(10, 100, "Input a number: ", "Incorrect! Try again: "));
-        System.out.println(inputHelper.parseInt(10, 100, "Input a number: ", "Incorrect! Try again: "));
-        System.out.println(inputHelper.parseInt(10, 100, "Input a number: ", "Incorrect! Try again: "));
+        int verticalSize = 0;
+        int horizontalSize = 0;
+
+        try {
+            if (args.length != 2) {
+                verticalSize = inputHelper.parseInt(1, 100,
+                        "Input a number: ", "Incorrect! Try again: ", 5);
+                horizontalSize = inputHelper.parseInt(1, 100,
+                        "Input a number: ", "Incorrect! Try again: ", 5);
+            } else {
+                verticalSize = inputHelper.parseIntFromString(args[0], 1, 100);
+                horizontalSize = inputHelper.parseIntFromString(args[1], 1, 100);
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println(ex.getMessage() + " Restart game!");
+            System.exit(1);
+        }
+
+        System.out.println(verticalSize);
+        System.out.println(horizontalSize);
+
     }
 }
--- Battleship/src/battleship/utilities/InputHelper.java
+++ Battleship/src/battleship/utilities/InputHelper.java
@@ -2,6 +2,7 @@ package battleship.utilities;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public final class InputHelper {
@@ -31,26 +32,40 @@ public final class InputHelper {
         this.out = outStream;
     }
 
-    public int parseInt(int from, int to, String prompt, String errorMessage) {
-        out.print(prompt);
-        String input = in.nextLine();
+    public int parseIntFromString(String input, int from, int to) {
+        int res = Integer.parseInt(input);
+        if (!(res >= from && res <= to)) {
+            throw new NumberFormatException("Incorrect input!");
+        }
 
+        return res;
+    }
+
+    public int parseInt(int from, int to, String prompt, String errorMessage, int attempts) {
+        out.print(prompt);
+        String input;
+
+        int current_attempt = 0;
         int res = 0;
         boolean isCorrect = false;
 
         while (!isCorrect) {
+            if (current_attempt >= attempts) {
+                throw new NumberFormatException("You did not enter correct number!");
+            }
+
             try {
+                input = in.nextLine();
                 res = Integer.parseInt(input);
                 if (res >= from && res <= to) {
                     isCorrect = true;
                 } else {
                     out.print(errorMessage);
-                    input = in.nextLine();
                 }
             } catch (NumberFormatException ex) {
                 out.print(errorMessage);
-                input = in.nextLine();
             }
+            ++current_attempt;
         }
 
         return res;
