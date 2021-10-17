52910e0cec68afa3a600041fcbc60b2639049c4a
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-10-17 21:10:01 +0300

    new main class added

---
+++ Battleship/src/battleship/BattleshipGame.java
@@ -0,0 +1,13 @@
+package battleship;
+
+public class BattleshipGame {
+
+    public static void main(String[] args) {
+        BattleshipGame gameSession = new BattleshipGame();
+        gameSession.playGame();
+    }
+
+    public void playGame() {
+        System.out.println("Game started!");
+    }
+}
