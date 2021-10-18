745894754cbd41119a13ba73d07bbe6f24795a05
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-10-18 14:46:20 +0300

    Ocean creation added

--- Battleship/src/battleship/Ocean.java
+++ Battleship/src/battleship/Ocean.java
@@ -1,2 +1,45 @@
-package battleship;public class Ocean {
+package battleship;
+
+
+public class Ocean {
+    private final int verticalSize;
+    private final int horizontalSize;
+    private final StringBuilder builder;
+
+    public Ocean(int verticalSize, int horizontalSize) {
+        this.verticalSize = verticalSize;
+        this.horizontalSize = horizontalSize;
+        builder = new StringBuilder();
+        createOcean();
+    }
+
+
+    public void print() {
+        System.out.println();
+        System.out.println(builder);
+    }
+
+    private void createOcean() {
+        builder.append("    ");
+        for (int i = 0; i < horizontalSize; ++i) {
+            builder.append(' ').append((char) ('a' + i));
+        }
+
+        builder.append('\n');
+        builder.append("  ┌");
+        builder.append("——".repeat(horizontalSize));
+        builder.append("———┐");
+        builder.append('\n');
+
+        for (int i = 0; i < verticalSize; ++i) {
+            builder.append(i + 1).append(" │ ");
+            builder.append(" ◎".repeat(horizontalSize));
+            builder.append("  │");
+            builder.append('\n');
+        }
+
+        builder.append("  └");
+        builder.append("——".repeat(horizontalSize));
+        builder.append("———┘");
+    }
 }
